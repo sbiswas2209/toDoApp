c7c323c67417e703665afdc9cb77f0f0f17d0806
Sagnik Biswas <60578512+sbiswas2209@users.noreply.github.com>
2020-10-07 17:06:43 +0530

    Added Picture support(JPEG)

--- src/pages/new-task/new-task.ts
+++ src/pages/new-task/new-task.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import { Camera , CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the NewTaskPage page.
@@ -18,9 +19,11 @@ export class NewTaskPage {
 
   public title: any;
   public content: any;
+  public picture: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams , public storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams , public storage: Storage, public camera: Camera) {
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewTaskPage');
@@ -33,11 +36,30 @@ export class NewTaskPage {
         val.push({
           title: this.title,
           content: this.content,
+          picture: this.picture,
         });
         this.storage.set('tasks', val);
       }
     );
       this.navCtrl.pop();
+  }
+
+  savePhoto(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data: image/jpeg;base64,' + imageData;
+      console.log(this.picture);
+    }, (err) => {
+      console.log(err);
+    });
+
   }
 
 }
